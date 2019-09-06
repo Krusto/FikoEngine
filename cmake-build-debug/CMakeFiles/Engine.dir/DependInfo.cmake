d5cf205da3a282f0ed01e78a4bea083e6d522d6c
Krusto <kuckata2@gmail.com>
2019-09-06 15:43:52 +0300

    commit

---
+++ cmake-build-debug/CMakeFiles/Engine.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Application.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Application.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Logger/lwlog/formatter.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Logger/lwlog/formatter.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Logger/lwlog/lwlog.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Logger/lwlog/lwlog.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Logger/lwlog/registry.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Logger/lwlog/registry.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Renderer/Platform/Vulkan/AllocationManger.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/AllocationManger.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Renderer/Platform/Vulkan/VulkanCore.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/VulkanCore.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Renderer/Platform/Vulkan/VulkanCreateInfos.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/VulkanCreateInfos.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Renderer/Platform/Vulkan/VulkanRendererAPI.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/VulkanRendererAPI.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Renderer/RenderCommand.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Renderer/RenderCommand.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Renderer/Renderer.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Renderer/Renderer.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Renderer/RendererAPI.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Renderer/RendererAPI.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Engine/Window.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Engine/Window.cpp.o"
+  "/home/krusto/CLionProjects/FikoEngine/Sandbox/src/Sandbox.cpp" "/home/krusto/CLionProjects/FikoEngine/cmake-build-debug/CMakeFiles/Engine.dir/Sandbox/src/Sandbox.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "VK_USE_PLATFORM_XLIB_KHR"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Engine.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engine.dir/Engine/Application.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Window.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Renderer/Renderer.cpp.o"
+  "CMakeFiles/Engine.dir/Sandbox/src/Sandbox.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Renderer/RendererAPI.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Renderer/RenderCommand.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/VulkanRendererAPI.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/VulkanCore.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Logger/lwlog/formatter.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Logger/lwlog/lwlog.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Logger/lwlog/registry.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/VulkanCreateInfos.cpp.o"
+  "CMakeFiles/Engine.dir/Engine/Renderer/Platform/Vulkan/AllocationManger.cpp.o"
+  "Engine.pdb"
+  "Engine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
