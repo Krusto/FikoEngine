a2f3a84b4bef0a6edc1010fd66035b58eb82ab1e
Krusto <k.stoianov2@gmail.com>
2022-06-23 16:33:22 +0300

    Fixed CMakeLists.txt and added VulkanMemoryAllocator library

---
+++ src/Renderer/Vulkan/Buffer.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Stoyanov, Krusto (K.S.) on 6/23/2022.
+//
+//#define VMA_IMPLEMENTATION
+//#include <vk_mem_alloc.h>
+
+#include "Buffer.h"
+
+namespace FikoEngine{
+
+}
--- src/Renderer/Vulkan/Buffer.h
+++ src/Renderer/Vulkan/Buffer.h
@@ -2,7 +2,12 @@
 // Created by Stoyanov, Krusto (K.S.) on 6/23/2022.
 //
 #include <vulkan/vulkan.h>
+//#include <vk_mem_alloc.h>
 
 namespace FikoEngine{
 
+//    struct AllocatedBuffer {
+//        VkBuffer _buffer;
+//        VmaAllocation _allocation;
+//    };
 }
