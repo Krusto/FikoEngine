80d91c7f80fe221823fb8a629f4519a6460eb456
Christian Panov <christianpanov@abv.bg>
2022-06-25 11:12:55 +0300

    Update ShaderDataType.h

--- src/Renderer/ShaderDataType.h
+++ src/Renderer/ShaderDataType.h
@@ -4,41 +4,33 @@
 #pragma once
 #include <vulkan/vulkan.h>
 
-namespace FikoEngine{
-    struct ShaderDataType {
+namespace FikoEngine
+{
+    struct ShaderDataType 
+    {
     public:
-        enum class ShaderType {vec1,vec2, vec3, vec4, };
-        static size_t Size(ShaderType Type) {
-            switch (Type) {
-                case ShaderType::vec2:
-                    return 2 * sizeof(float);
-                    break;
-                case ShaderType::vec3:
-                    return 3 * sizeof(float);
-                    break;
-                case ShaderType::vec4:
-                    return 4 * sizeof(float);
-                    break;
-                case ShaderType::vec1:
-                    return sizeof(float);
-                    break;
+        enum class ShaderType { vec1, vec2, vec3, vec4, }; // Can be optimized. The enum type could be set to u8, since the values are only 4
+        
+        static std::size_t Size(ShaderType type) 
+        {
+            switch (type) 
+            {
+                case ShaderType::vec2: return 2 * sizeof(float); // NO NEED TO BREAK, SINCE YOU ARE RETURNING
+                case ShaderType::vec3: return 3 * sizeof(float);
+                case ShaderType::vec4: return 4 * sizeof(float);
+                case ShaderType::vec1: return sizeof(float);    
             }
             return 0;
         }
-        static VkFormat Format(ShaderType Type) {
-            switch (Type) {
-                case ShaderType::vec2:
-                    return VK_FORMAT_R32G32_SFLOAT;
-                    break;
-                case ShaderType::vec3:
-                    return VK_FORMAT_R32G32B32_SFLOAT;
-                    break;
-                case ShaderType::vec4:
-                    return VK_FORMAT_R32G32B32A32_SFLOAT;
-                    break;
-                case ShaderType::vec1:
-                    return VK_FORMAT_R32_SFLOAT;
-                    break;
+        
+        static VkFormat Format(ShaderType Typtypee) 
+        {
+            switch (type) 
+            {
+                case ShaderType::vec2: return VK_FORMAT_R32G32_SFLOAT; // NO NEED TO BREAK, SINCE YOU ARE RETURNING
+                case ShaderType::vec3: return VK_FORMAT_R32G32B32_SFLOAT;
+                case ShaderType::vec4: return VK_FORMAT_R32G32B32A32_SFLOAT;
+                case ShaderType::vec1: return VK_FORMAT_R32_SFLOAT;
             }
             return VK_FORMAT_UNDEFINED;
         }
