46ff1cbe9dc9af5e91f1a04656ce0c12b0865688
Christian Panov <christianpanov@abv.bg>
2022-06-29 19:24:27 +0300

    Update UniformLayout.h

--- src/Renderer/UniformLayout.h
+++ src/Renderer/UniformLayout.h
@@ -5,24 +5,26 @@
 
 #include "../Core/Core.h"
 #include "UniformDataType.h"
-namespace FikoEngine{
-    struct UniformAttribute {
+namespace FikoEngine
+{
+    struct UniformAttribute 
+    {
     public:
-        std::string_view name{};
+        std::string_view name;
         UniformDataType::UniformType type;
         u32 offset{};
     };
 
-    class UniformLayout {
+    class UniformLayout 
+    {
     public:
         UniformLayout() = default;
         UniformLayout(std::initializer_list<UniformAttribute> attributes);
         UniformLayout(const UniformLayout&) = default;
-
+        UniformLayout& operator=(const UniformLayout& other) = default;
+        
+    public:
         std::vector<UniformAttribute> attributes;
         u32 stride{};
-
-        UniformLayout& operator=(const UniformLayout& other) = default;
-
     };
 }
