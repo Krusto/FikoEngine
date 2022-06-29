bab45a31d5bc52e74bc547cd7b1fdcf0998dca1d
Christian Panov <christianpanov@abv.bg>
2022-06-29 19:24:17 +0300

    Update UniformLayout.cpp

--- src/Renderer/UniformLayout.cpp
+++ src/Renderer/UniformLayout.cpp
@@ -3,13 +3,15 @@
 //
 #include "UniformLayout.h"
 #include "../Core/Core.h"
-namespace FikoEngine{
-
-    FikoEngine::UniformLayout::UniformLayout(std::initializer_list<UniformAttribute> attributes) : attributes(attributes){
-        for (auto& [name,type,offset] : this->attributes)
+namespace FikoEngine
+{
+    FikoEngine::UniformLayout::UniformLayout(std::initializer_list<UniformAttribute> attributes) 
+        : attributes{ attributes }
+    {
+        for (auto& [name, type, offset] : this->attributes)
         {
             offset = stride;
-            stride += (u32)UniformDataType::Size(type);
+            stride += static_cast<u32>(UniformDataType::Size(type));
         }
     }
 }
